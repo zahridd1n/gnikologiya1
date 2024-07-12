4003183fee75b39d28ebefee124f505f1bdaaeb0
Ubuntu <ubuntu@ip-172-31-26-96.ec2.internal>
2024-07-12 11:26:49 +0000

    Asosiy loyiha kodlari

--- core/settings.py
+++ core/settings.py
@@ -139,6 +139,17 @@ DATABASES = {
     }
 }
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'myproject',
+#        'USER': 'myprojectuser',
+#        'PASSWORD': 'password',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+#}
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 # settings.py
